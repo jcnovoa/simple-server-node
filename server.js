cb89063f89514cfe6b31c7a571ae7676742dff1a
J.C. Novoa <jcnovoa@hotmail.com>
2016-11-03 13:27:47 -0400

    Update port
    Update Master

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello IoT World!');
 });
 
 //start a server on port 80 and log its start to our console
-var server = app.listen(80, function () {
+var server = app.listen(4000, function () {
 
   var port = server.address().port;
-  console.log('Example app listening on port ', port);
+  console.log('MyPIoT is listening on port: ', port);
 
 });
