bd2281901b3551f295539b9105eef8b023445a83
J.C. Novoa <jcnovoa@hotmail.com>
2016-11-05 14:22:18 -0400

    Johnny-Five
    Added board

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var five = require("johnny-five");
+var Edison = require("edison-io");
+var board = new five.Board();
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
@@ -7,9 +10,16 @@ app.get('/', function (req, res) {
 });
 
 //start a server on port 80 and log its start to our console
-var server = app.listen(4000, function () {
 
-  var port = server.address().port;
-  console.log('MyPIoT is listening on port: ', port);
+board.on("ready", function () {
+
+    var server = app.listen(4000, function () {
+
+        var port = server.address().port;
+        console.log('MyPIoT is listening on port: ', port);
+
+    });
 
 });
+
+
